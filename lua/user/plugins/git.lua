693d9f8c5caaa856a985eb9c0b3eee6d8334a7ab
yohans <emailyyohans@gmail.com>
2025-04-24 22:37:31 +0300

    commited from neogit

--- lua/user/plugins/git.lua
+++ lua/user/plugins/git.lua
@@ -6,4 +6,16 @@ return {
 		end,
 	},
 	{ "sindrets/diffview.nvim" },
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+			"echasnovski/mini.pick", -- optional
+		},
+	},
 }
