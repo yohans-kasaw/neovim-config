c21b6530a73de129ae6f0651945bc4552f3aa5a7
yohans <emailyyohans@gmail.com>
2025-05-11 14:50:56 +0300

    feat(codecompanion): add key maps for actions chat and adding code to chat

--- lua/user/core/trial.lua
+++ lua/user/core/trial.lua
@@ -1,6 +1,12 @@
 return {
 	plugins = {},
 	keys = function()
+		vim.keymap.set("n", "<leader>c", "<cmd>CodeCompanionChat Toggle<CR>", { noremap = true, silent = false })
+		vim.keymap.set("v", "<leader>c", "<cmd>CodeCompanionChat Add<CR>", { noremap = true, silent = false })
+		vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionAction<CR>", { noremap = true, silent = false })
+		-- Expand 'cc' into 'CodeCompanion' in the command line
+		vim.cmd([[cab cc CodeCompanion]])
+
 		vim.keymap.set("n", "<leader>w", function()
 			local win_id = vim.api.nvim_get_current_win()
 			local current_width = vim.api.nvim_win_get_width(win_id)
