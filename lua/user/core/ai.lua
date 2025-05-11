95b13e936b8cb6af4e14b29e8714607c7eb29fee
yohans <emailyyohans@gmail.com>
2025-05-11 15:11:03 +0300

    feat(ai): add openrouter to code companion

--- lua/user/core/ai.lua
+++ lua/user/core/ai.lua
@@ -7,6 +7,35 @@ return {
 				"nvim-lua/plenary.nvim",
 				"nvim-treesitter/nvim-treesitter",
 			},
+
+			config = function()
+				require("codecompanion").setup({
+					strategies = {
+						chat = {
+							adapter = "openrouter",
+						},
+						inline = {
+							adapter = "openrouter",
+						},
+					},
+					adapters = {
+						openrouter = function()
+							return require("codecompanion.adapters").extend("openai_compatible", {
+								env = {
+									url = "https://openrouter.ai/api",
+									api_key = "OPENROUTER_API_KEY",
+									chat_url = "/v1/chat/completions",
+								},
+								schema = {
+									model = {
+										default = 'google/gemini-2.0-flash-001',
+									},
+								},
+							})
+						end,
+					},
+				})
+			end,
 		},
 	},
 	keys = function() end,
