6f17a2aa1f2a37a32cbd6cc3d3599ac00211fa05
yohans-kasaw <emailyohans@gmail.com>
2025-06-03 22:29:54 +0300

    added leap

--- lua/user/core/motion.lua
+++ lua/user/core/motion.lua
@@ -1,8 +1,26 @@
 return {
 	plugins = {
 		{ "aaronik/treewalker.nvim" },
+		{
+			"ggandor/leap.nvim",
+			config = function()
+				require("leap").setup({
+					safe_labels = {},
+					preview_filter = function()
+						return false
+					end,
+				})
+			end,
+		},
 	},
 	keys = function()
+		vim.keymap.set({ "x", "o" }, "s", "<Plug>(leap)")
+		vim.keymap.set({ "n", "x", "o" }, "gs", function()
+			require("leap.remote").action()
+		end)
+
+		vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+		vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
 		vim.keymap.set({ "n", "v", "o" }, "H", "0", { noremap = true })
 		vim.keymap.set({ "n", "v", "o" }, "L", "$", { noremap = true })
 
